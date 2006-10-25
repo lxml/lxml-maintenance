5a12b0470ac77803d8867d53bca56af0e07f27be
scoder <none@none>
2006-10-25 19:22:09 +0200

    [svn r1986] src-local Pyrex
    --HG--
    branch : lxml-1.1

--- setup.py
+++ setup.py
@@ -100,6 +100,11 @@ print "Building lxml version", svn_version
 
 # setup etree extension building
 
+if '--no-local-pyrex' in sys.argv:
+    sys.argv.remove('--no-local-pyrex')
+else:
+    sys.path.insert(0, os.path.join(src_dir, 'src')) # in case we use a local Pyrex
+
 try:
     from Pyrex.Distutils import build_ext as build_pyx
     source_extension = ".pyx"
